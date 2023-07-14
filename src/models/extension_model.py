bad31b4dcd62603891015b9a6646d77e33adc9a2
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-07-15 01:13:25 +0700

    update model and schema

--- src/models/extension_model.py
+++ src/models/extension_model.py
@@ -8,6 +8,7 @@ class ExtensionModel(Document):
     agent = fields.StringField(unique=True, allow_none=True)
     status = fields.StringField(allow_none=True)
     description = fields.StringField(allow_none=True)
+    tenant = fields.StringField(allow_none=True)
 
     class Meta:
         collection_name = "Extensions"
--- src/schemas/extension_schema.py
+++ src/schemas/extension_schema.py
@@ -6,6 +6,7 @@ class ExtensionSchema(BaseModel):
     agent: str | None = None
     status: str | None = None
     description: str | None = None
+    tenant: str | None = None
 
     @field_validator("extension_id")
     def id_must_contain_numbers_only(cls, v):
@@ -15,15 +16,31 @@ class ExtensionSchema(BaseModel):
     
     @field_validator("status")
     def must_have_valid_status(cls, v):
-        if v != "Enable" and v != "Disable":
+        if v != "Enable" and v != "Disable" and v is not None:
             raise ValueError("Invalid status")
         return v
-        
+
+class ExtensionUpdateSchema(BaseModel):
+    extension_id: None = None
+    list_queue_id: list[str] | None = None
+    agent: str | None = None
+    status: str | None = None
+    description: str | None = None
+    tenant: str | None = None
+
+    @field_validator("status")
+    def must_have_valid_status(cls, v):
+        if v != "Enable" and v != "Disable" and v is not None:
+            raise ValueError("Invalid status")
+        return v
+
 
 if __name__ == "__main__":
     try:
         print(ExtensionSchema(extension_id="1123", list_queue_id=[], agent="namnt134", status="Enable", description=None))
         print(ExtensionSchema(extension_id="1123", list_queue_id=[], agent="namnt134", status="Enable", description=None).model_dump_json())
+        print(ExtensionUpdateSchema(list_queue_id=[], agent="namnt134", status=None, description=None))
+        
 
     except ValidationError as e:
         print(e)
